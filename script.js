0d2b3241095b6562f0ece13d0cca5f9ee3fc2264
Ariff M <munshicorp@gmail.com>
2023-10-14 16:25:32 +0800

    Add responive css

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const displayUser = (user) => {
     const template = document.getElementById("card-template").content.cloneNode(true);
     
     template.querySelector('.card-title').innerText = user.name;
-    template.querySelector('.card-text').innerText = user.email;
+    template.querySelector('.card-username').innerText = user.username;
+    template.querySelector('.card-email').innerText = user.email;
 
     document.querySelector('#user-list').appendChild(template);
 }
